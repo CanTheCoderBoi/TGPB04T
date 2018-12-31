8e71005f4adda8e45116cc800ca7aca4e48604e3
Can <canerdal2003@hotmail.com>
2018-12-31 19:41:07 +0300

    added basic files + docs command

---
+++ bot.js
@@ -0,0 +1,19 @@
+const discord = require("discord.js")
+const client = new discord.Client();
+const pref = "?";
+
+client.on("ready", () => {
+    console.log("I am up and running!");
+})
+
+client.on("message", async (message) => {
+    if (message.author.bot) return;
+    if (message.channel.type === "dm") return;
+    if (!message.content.startsWith(pref)) return;
+    const args = message.content.slice(pref.length).trim().split(/ +/g);
+    const command = args.shift().toLowerCase();
+    let commandFile = require(`./cmds/${command}.js`);
+    return commandFile.run(client, message, args);
+})
+
+client.login(process.env.TOKEN)
