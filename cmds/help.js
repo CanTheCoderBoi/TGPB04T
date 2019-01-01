587f99472d9e8cca17da681ed66b9c994bc7384c
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-01 16:13:16 +0300

    Hmm

--- cmds/help.js
+++ cmds/help.js
@@ -12,10 +12,10 @@ module.exports.run = (client, message, args) => {
     let helparg = args[0]
     
     if(helparg === "class") {
-        let classembed = new Discord.RichEmbed()
+        let classembed = new discord.RichEmbed()
         classembed.setAuthor(message.author.tag, message.author.avatarURL);
-        classembed.setTitle("yo")
-        classembed.setDescription("hmm");
+        classembed.setTitle("Class komutları: ")
+        classembed.setDescription("Attachment  CategoryChannel  Channel  Client  ClientUser  ClientUserChannelOverride  ClientUserGuildSettings  ClientUserSettings  Collection  Collector  DiscordAPIError  DMChannel  Emoji  EvaluatedPermissions  Game  GroupDMChannel  Guild  GuildAuditLogs  GuildAuditLogsEntry  GuildChannel  GuildMember  Invite  Message  MessageAttachment  MessageCollector  MessageEmbed  MessageEmbedAuthor  MessageEmbedField  MessageEmbedFooter  MessageEmbedImage  MessageEmbedProvider  MessageEmbedThumbnail  MessageEmbedVideo  MessageMentions  MessageReaction  OAuth2Application  PartialGuild  PartialGuildChannel  PermissionOverwrites  Permissions  Presence  ReactionCollector  ReactionEmoji  RichEmbed  RichPresenceAssets  Role  Shard  ShardClientUtil  ShardingManager  SnowflakeUtil  StreamDispatcher  TextChannel  User  UserConnection  UserProfile  Util  VoiceBroadcast  VoiceChannel  VoiceConnection  VoiceReceiver  VoiceRegion  VolumeInterface  Webhook  WebhookClient  ");
         classembed.setFooter("Komutlar çalışmıyorsa büyük/küçük harf uyumuna dikkat ediniz.")
         message.channel.send(classembed);        
     }
