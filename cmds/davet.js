4dfd4eefc723bc11a64e8783c9407ce618c5a0c3
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-01 19:43:27 +0300

    Update davet.js

--- cmds/davet.js
+++ cmds/davet.js
@@ -1,4 +1,4 @@
 const discord = require("discord.js");
 module.exports.run = (client, message, args) => {
- message.channel.send("<:yesil:527600859513618432> Kalıcı davet linki :  **https://discord.gg/tRZuCYj** ")
+ message.channel.send("<:yesil:527600859513618432> Kalıcı davet linki :  **<https://discord.gg/tRZuCYj>** ")
 }
