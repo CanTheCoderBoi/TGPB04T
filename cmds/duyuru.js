c1bb796059a4e7013457146d54c4fc631063c6bd
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-01 16:34:26 +0300

    duyuru

---
+++ cmds/duyuru.js
@@ -0,0 +1,16 @@
+const discord = require("discord.js");
+module.exports.run = (client, message, args) => {
+
+
+        message.delete()
+        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Bunu kullanmak için yetkiniz yok")
+        var dtext = message.content.split(' ').slice(1).join(' ')
+        if(!dtext) return message.reply('Metin girişi yapılmadı. Lütfen metin giriniz.')
+        
+        let duyuruembed = new discord.RichEmbed()
+        duyuruembed.setDescription(dtext)
+        duyuruembed.setThumbnail(message.author.avatarURL)
+        duyuruembed.setColor(0x00FFFF)       
+        message.channel.send(duyuruembed);        
+        }
+      
