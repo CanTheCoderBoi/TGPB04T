9f8f37d2c33e9b488045b0a0317f0cb82999cc9a
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-01 15:56:26 +0300

    class embed

--- cmds/help.js
+++ cmds/help.js
@@ -10,10 +10,25 @@ module.exports.run = (client, message, args) => {
     }
     if(args.length>0){
     let helparg = args[0]
-    if(helparg === "class") {
-        message.channel.send("That works beep boop");
-        
     
+    if(helparg === "class") {
+        let classembed = new Discord.RichEmbed()
+        classembed.setAuthor(message.author.tag, message.author.avatarURL);
+        classembed.addField("Alfabetik sıraya göre class komutları","Attachment  CategoryChannel  Channel  Client  ClientUser  ClientUserChannelOverride  ClientUserGuildSettings  ClientUserSettings  Collection  Collector  DiscordAPIError  DMChannel  Emoji  EvaluatedPermissions  Game  GroupDMChannel  Guild  GuildAuditLogs  GuildAuditLogsEntry  GuildChannel  GuildMember  Invite  Message  MessageAttachment  MessageCollector  MessageEmbed  MessageEmbedAuthor  MessageEmbedField  MessageEmbedFooter  MessageEmbedImage  MessageEmbedProvider  MessageEmbedThumbnail  MessageEmbedVideo  MessageMentions  MessageReaction  OAuth2Application  PartialGuild  PartialGuildChannel  PermissionOverwrites  Permissions  Presence  ReactionCollector  ReactionEmoji  RichEmbed  RichPresenceAssets  Role  Shard  ShardClientUtil  ShardingManager  SnowflakeUtil  StreamDispatcher  TextChannel  User  UserConnection  UserProfile  Util  VoiceBroadcast  VoiceChannel  VoiceConnection  VoiceReceiver  VoiceRegion  VolumeInterface  Webhook  WebhookClient");
+        classembed.setFooter("Komutlar çalışmıyorsa büyük/küçük harf uyumuna dikkat ediniz.")
+        message.channel.send(classembed);        
     }
+        
+        
+        
+        
+        
     }
+    
+    
+    
+    
+    
+    
+    
     } 
