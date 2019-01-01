74a5ccd4239420b463ab2178e9d36228ddfdfeea
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-01 19:42:39 +0300

    Create davet.js

---
+++ cmds/davet.js
@@ -0,0 +1,4 @@
+const discord = require("discord.js");
+module.exports.run = (client, message, args) => {
+ message.channel.send("<:yesil:527600859513618432> Kalıcı davet linki :  **https://discord.gg/tRZuCYj** ")
+}
