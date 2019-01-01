a0da23af92044c2fa6d9ab1c7a8e712e241cec81
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-01 16:48:03 +0300

    Update help.js

--- cmds/help.js
+++ cmds/help.js
@@ -3,6 +3,7 @@ module.exports.run = (client, message, args) => {
     if (args.length == 0) {
     let helpembed = new discord.RichEmbed();
     helpembed.setAuthor(message.author.tag, message.author.avatarURL);
+    helpembed.addField("?docs <başlık>","Belirlenen dosyanın anlatımına götürür. Örnek olarak class başlığının alt başlığı olan Attachments i görmek için ?docs Attachments")
     helpembed.addField("?help class","Class bilgisini gösterir")
     helpembed.setThumbnail("https://discord.js.org/static/logo-square.png")
     helpembed.setFooter("Komutlar ile ilgili daha fazla bilgi almak için belirtilen komudu giriniz")
