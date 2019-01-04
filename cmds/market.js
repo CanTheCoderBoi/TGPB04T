afeaed2b91b98e4d070d71cdc2ed0d189ed187d2
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-04 21:03:24 +0300

    Update market.js

--- cmds/market.js
+++ cmds/market.js
@@ -3,8 +3,12 @@ module.exports.run = (client, message, args) => {
 let marketembed = new discord.RichEmbed()
 marketembed.setAuthor(message.author.tag, message.author.avatarURL);
 marketembed.setTitle("TGP Market")
-marketembed.addField("<@&529706013905911811>","<@&529706013905911811>")
-marketembed.setFooter("yoo")
+marketembed.addField("Regular","<@&529706013905911811> rolünü kazanırsınız. Rolde ekstradan nick değişme hakkı sunulur. Fiyat : 15 coin. ")
+marketembed.addField("Veteran","<@&529706657605877772> rolünü kazanırsınız. Fiyat : 25 coin. ")
+marketembed.addField("TGP Specialist","<@&529047857500520458> rolünü kazanırsınız. Fiyat : 50 coin. ")
+marketembed.addField("Proficient","<@&523181300857962506> rolünü kazanırsınız. Fiyat : 100 coin. ")
+marketembed.setFooter("Markette sorun olursa C4Navar#1337 ' a mesaj atınız.")
+ marketembed.setTimsStamp()
 message.channel.send(marketembed)
 
 }
