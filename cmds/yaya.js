9d7eb44f25a3d7422a3c8e93ab6e909cebbf7c47
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-05 14:37:28 +0300

    Create yaya.js

---
+++ cmds/yaya.js
@@ -0,0 +1,6 @@
+const discord = require("discord.js");
+module.exports.run = (client, message, args) => {
+message.delete()
+message.channel.send("**Kalıcı davet linki: https://discord.gg/tRZuCYj**)
+
+}
