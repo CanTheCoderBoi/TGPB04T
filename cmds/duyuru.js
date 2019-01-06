f03447ada281493060bff7e6e44ec37e0323d119
Can <canerdal2003@hotmail.com>
2019-01-06 12:26:27 +0300

    Küçük düzeltmeler.

--- cmds/duyuru.js
+++ cmds/duyuru.js
@@ -4,14 +4,14 @@ module.exports.run = (client, message, args) => {
 
         message.delete()
         if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Bunu kullanmak için yetkiniz yok")
-        var dtext = message.content.split(' ').slice(1).join(' ')
+        var dtext = args.join(' ')
         if(!dtext) return message.reply('Metin girişi yapılmadı. Lütfen metin giriniz.')
         
         let duyuruembed = new discord.RichEmbed()
         duyuruembed.setDescription(dtext)
         
         duyuruembed.setColor(0x00FFFF)       
-     message.channel.send("@here")
+     message.channel.send("@Ping")
         message.channel.send(duyuruembed);        
         }
       
