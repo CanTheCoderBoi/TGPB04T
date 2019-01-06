10a372c4bc8ccb56de3f3a62d54010499b35f8a3
Can <canerdal2003@hotmail.com>
2019-01-06 12:25:02 +0300

    Küçük düzeltmeler.

--- cmds/al.js
+++ cmds/al.js
@@ -11,33 +11,37 @@ module.exports.run = (client, message, args) => {
         } else {
             let item = args.join(" ").toLowerCase().toString();
             switch (item) {
-                case "regular" || "1":
-                    if (user.points < 15) return message.channel.send("Üzgünüm ama yeterli miktarda puanınız bulunmamaktadır.")
-                    let regular = message.guild.roles.get("529706013905911811")
+                case ("regular" || "1"):
+                if (user.points < 15) return message.channel.send("Üzgünüm ama yeterli miktarda puanınız bulunmamaktadır.")
+                let regular = message.guild.roles.get("529706013905911811")
+                if(message.member.roles.has(regular)) return message.reply("Bu role zaten sahipsin!")
                     user.points -= 15;
                     message.member.addRole(regular);
                     message.reply("Satın Alım Başarılı! 15 <:tgpcoin:530810516629618718> karşılığında \`Regular\` Rolünü satın aldın!")
                     user.save()
                     break;
-                case "veteran" || "2":
+                case ("veteran" || "2"):
                     if (user.points < 30) return message.channel.send("Üzgünüm ama yeterli miktarda puanınız bulunmamaktadır.")
                     let veteran = message.guild.roles.get("529706657605877772")
+                    if (message.member.roles.has(veteran)) return message.reply("Bu role zaten sahipsin!")
                     user.points -= 30;
                     message.member.addRole(veteran);
                     message.reply("Satın Alım Başarılı! 30 <:tgpcoin:530810516629618718> karşılığında \`Veteran\` Rolünü satın aldın!")
                     user.save()
                     break;
-                case "tgp specialist" || "3" || "specialist":
+                case ("tgp specialist" || "3" || "specialist"):
                     if (user.points < 60) return message.channel.send("Üzgünüm ama yeterli miktarda puanınız bulunmamaktadır.")
                     let specialist = message.guild.roles.get("529047857500520458")
+                    if (message.member.roles.has(specialist)) return message.reply("Bu role zaten sahipsin!")
                     user.points -= 60;
                     message.member.addRole(specialist);
                     message.reply("Satın Alım Başarılı! 60 <:tgpcoin:530810516629618718> karşılığında \`TGP Specialist\` Rolünü satın aldın!")
                     user.save()
                     break;
-                case "proficient" || "4":
+                case ("proficient" || "4"):
                     if (user.points < 100) return message.channel.send("Üzgünüm ama yeterli miktarda puanınız bulunmamaktadır.")
                     let proficient = message.guild.roles.get("523181300857962506")
+                    if (message.member.roles.has(proficient)) return message.reply("Bu role zaten sahipsin!")
                     user.points -= 100;
                     message.member.addRole(proficient);
                     message.reply("Satın Alım Başarılı! 100 <:tgpcoin:530810516629618718> karşılığında \`Proficient\` Rolünü satın aldın!")
