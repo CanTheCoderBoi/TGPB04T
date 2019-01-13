31bd66fde6c0ef88b4e1601656e99f34643b493f
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-13 23:59:58 +0300

    Update linkler.js

--- cmds/linkler.js
+++ cmds/linkler.js
@@ -1,4 +1,4 @@
 const discord = require("discord.js");
 module.exports.run = (client, message, args) => {
-    message.channel.send("**Discord:** <https://discord.gg/tRZuCYj>\nTwitter: <https://twitter.com/tgplatformu>");
+    message.channel.send("**Discord:** <discord.gg/tRZuCYj>\nTwitter: https://twitter.com/tgplatformu");
 }
