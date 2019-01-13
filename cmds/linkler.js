64a39c57e5ebdef487d3b69afff9aa72a7331280
Can <canerdal2003@hotmail.com>
2019-01-13 20:58:07 +0300

    Added linkler.js

---
+++ cmds/linkler.js
@@ -0,0 +1,4 @@
+const discord = require("discord.js");
+module.exports.run = (client, message, args) => {
+    message.channel.send("**Discord:** <https://discord.gg/tRZuCYj>\nTwitter: <https://twitter.com/tgplatformu>");
+}
