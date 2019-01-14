90ea60c13bf6d77d4972a1958d7b1ed6ca207482
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-14 19:58:15 +0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -4,7 +4,7 @@ module.exports.run = (client, message, args) => {
 let bankisi = message.mentions.users.first()
 if(!bankisi) return message.channel.send("Sunucudan yasaklayacağım üyeyi seçmediniz.");
 let bansebep = args.slice(1).join(" ")
-if(!message.member.hasPermission("MANAGE_MESSAGES)) return message.channel.send("Bu komudu kullanmak için yetkiniz yok.")
+if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("Bu komudu kullanmak için yetkiniz yok.")
 message.guild.member(bankisi).ban(bansebep)
 client.channels.get("534411959723098122").send(`**${bankisi.tag}** (`${bankisi.id}`) kullanıcısı, **${message.author.tag}** (`${message.author.id}`) tarafından yasaklandı. Yasaklama sebebi : *${bansebep}*  `)
 
