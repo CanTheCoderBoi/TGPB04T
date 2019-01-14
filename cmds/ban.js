18cf655df2bccaac61fa467747ef3c6bf3c6e884
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-14 20:32:40 +0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -8,5 +8,6 @@ if(!message.member.roles.has("523179624772010004")) return message.channel.send(
 message.channel.send("Başarıyla **" + bankisi.tag + "** (`"+bankisi.id+"`) kullanıcısını sunucudan yasakladım." )
 message.guild.member(bankisi).ban(bansebep)
 client.channels.get("534411959723098122").send("**" + bankisi.tag + "**" + " (`" + bankisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan yasaklandı. Yasaklama sebebi : (`" +bansebep+ "`)" )
+client.channels.get("529042995769507840").send("**" + bankisi.tag + "**" + " (`" + bankisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan yasaklandı. Yasaklama sebebi : (`" +bansebep+ "`)" )
 
 }
