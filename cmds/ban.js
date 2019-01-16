bc6095ec5286c10b960e3990ca1526c130074a7f
Can <32599057+CanTheCoderBoi@users.noreply.github.com>
2019-01-16 18:05:32 +0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -7,7 +7,7 @@ if(!message.member.roles.has("523179624772010004")) return message.channel.send(
 if(!bankisi) return message.channel.send("Sunucudan yasaklayacağım üyeyi seçmediniz.");
 let bansebep = args.slice(1).join(" ")
 
-message.channel.send("Başarıyla **" + bankisi.tag + "** (`"+bankisi.id+"`) kullanıcısını sunucudan yasakladım." )
+message.channel.send("Başarıyla **" + bankisi.user.tag + "** (`"+bankisi.id+"`) kullanıcısını sunucudan yasakladım." )
 bankisi.ban(bansebep + "by : " +message.author.tag)
 client.channels.get("534411959723098122").send("**" + bankisi.user.tag + "**" + " (`" + bankisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan yasaklandı. Yasaklama sebebi : (`" +bansebep+ "`)" )
 client.channels.get("529042995769507840").send("**" + bankisi.user.tag + "**" + " (`" + bankisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan yasaklandı. Yasaklama sebebi : (`" +bansebep+ "`)" )
