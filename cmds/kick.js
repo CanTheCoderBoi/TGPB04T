2f6bb0590e8bcdd67e45324d61c9111ec38b9f77
Can <32599057+CanTheCoderBoi@users.noreply.github.com>
2019-01-16 18:04:23 +0300

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -4,15 +4,15 @@ module.exports.run = (client, message, args) => {
     const discord = require("discord.js");
     module.exports.run = (client, message, args) => {
     
-    let kickkisi = message.mentions.users.first()
+    let kickkisi = message.mentions.members.first()
     if(!kickkisi.kickable) return message.reply(" Maalesef bu kişi sunucudan atılamayacak kadar havalı.")
     if(!message.member.roles.has("523179624772010004")) return message.channel.send("Bu komudu kullanmak için yetkiniz yok.")
     if(!kickkisi) return message.channel.send("Sunucudan atacağım üyeyi seçmediniz.");
     let kicksebep = args.slice(1).join(" ")
     message.channel.send("Başarıyla **" + kickkisi.tag + "** (`"+kickkisi.id+"`) kullanıcısını sunucudan attım." )
     message.guild.member(kickkisi).kick(kicksebep + "by : " +message.author.tag)
-    client.channels.get("534411959723098122").send("**" + kickkisi.tag + "**" + " (`" + kickkisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan atıldı. Atılma sebebi : (`" +kicksebep+ "`)" )
-    client.channels.get("529042995769507840").send("**" + kickkisi.tag + "**" + " (`" + kickkisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan atıldı. Atılma sebebi : (`" +kicksebep+ "`)" )
+    client.channels.get("534411959723098122").send("**" + kickkisi.user.tag + "**" + " (`" + kickkisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan atıldı. Atılma sebebi : (`" +kicksebep+ "`)" )
+    client.channels.get("529042995769507840").send("**" + kickkisi.user.tag + "**" + " (`" + kickkisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan atıldı. Atılma sebebi : (`" +kicksebep+ "`)" )
     
     }
 
