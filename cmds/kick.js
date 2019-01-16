1ca9794c99112fa5ebd36f5a13848708c48616ad
Can <32599057+CanTheCoderBoi@users.noreply.github.com>
2019-01-16 18:05:58 +0300

    Update kick.js

--- cmds/kick.js
+++ cmds/kick.js
@@ -9,8 +9,8 @@ module.exports.run = (client, message, args) => {
     if(!message.member.roles.has("523179624772010004")) return message.channel.send("Bu komudu kullanmak için yetkiniz yok.")
     if(!kickkisi) return message.channel.send("Sunucudan atacağım üyeyi seçmediniz.");
     let kicksebep = args.slice(1).join(" ")
-    message.channel.send("Başarıyla **" + kickkisi.tag + "** (`"+kickkisi.id+"`) kullanıcısını sunucudan attım." )
-    message.guild.member(kickkisi).kick(kicksebep + "by : " +message.author.tag)
+    message.channel.send("Başarıyla **" + kickkisi.user.tag + "** (`"+kickkisi.id+"`) kullanıcısını sunucudan attım." )
+    kickkisi.kick(kicksebep + "by : " +message.author.tag)
     client.channels.get("534411959723098122").send("**" + kickkisi.user.tag + "**" + " (`" + kickkisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan atıldı. Atılma sebebi : (`" +kicksebep+ "`)" )
     client.channels.get("529042995769507840").send("**" + kickkisi.user.tag + "**" + " (`" + kickkisi.id + "`)" + " kullanıcısı , **" + message.author.tag + "** (`" + message.author.id + "`) tarafından sunucudan atıldı. Atılma sebebi : (`" +kicksebep+ "`)" )
     
