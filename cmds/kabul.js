0cb9dd448829343bed27fbc4da4b0b3961794c3f
Can <canerdal2003@hotmail.com>
2019-01-18 17:24:21 +0300

    Partnerlik ile ilgili bir kaç düzenleme.

---
+++ cmds/kabul.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+module.exports.run = (client, message, args) => {
+    if (message.channel.id != "531395962904641538") return;
+    if(!args[0]) return message.channel.send("Lütfen mesaj kodunu belirtiniz.")
+    message.channel.fetchMessage(args[0]).then(msg => {
+        let user = client.users.find("tag", msg.embeds[0].author.name);
+        user.send("Partnerlik Başvurunuz onaylanmıştır! Lütfen aşağıda belirttiğim metni kopyalayıp kendi sunucunuzda ilgili yere yapıştırınız."+ "```\n**Türk Geliştiriciler Platformu(TGP)**\nKodlamayı seviyorsanız kesinlikle bize katılmalısınız! TGP, Türk yazılımcıları ve geliştiricleri bir araya getirip aralarında bir bağ oluşturmaya dayalı bir sunucudur.\nSunucuda Neler Var ?\n• Kendine özel rol ve puan sistemi.\n• Neredeyse her programlama dili için özel kanallar.\n• Aktif Kitle\n• Discord Botunuzu geliştirmeye yardımcı olacak kişiler\n• Özel emojiler\n• Çekilişler\n\nVe daha fazlası!\n**Hemen Katılmak İçin Tıkla:** https://discord.gg/tRZuCYj```").catch(err => {
+            return message.channel.send(`\`${args[0]}\` kabul edildi. Gerekli kişiye bilgilendirme yapılırken hata oluştu.`);
+        })
+        return message.channel.send(`\`${args[0]}\` kabul edildi. Gerekli kişiye bilgilendirme yapıldı.`);
+    })
+}
+
+
---
+++ cmds/red.js
@@ -0,0 +1,14 @@
+const discord = require("discord.js");
+module.exports.run = (client, message, args) => {
+    if (message.channel.id != "531395962904641538") return;
+    if(!args[0]) return message.channel.send("Lütfen mesaj kodunu belirtiniz.")
+    message.channel.fetchMessage(args[0]).then(msg => {
+        let user = client.users.find("tag", msg.embeds[0].author.name);
+        user.send("Üzgünüm, Partnerlik başvurunuz yetkililerimiz tarafından gözden geçirilmiş ve onaylanmamıştır. Bazı nedenleri şunlar olabilir:\n Sunucuda bulunan üye azlığı.\nKanallarda aktiflik olmaması.\nSunucu düzeninin karışık olması.\n").catch(err => {
+            return message.channel.send(`\`${args[0]}\` reddedildi. Gerekli kişiye bilgilendirme yapılırken hata oluştu.`)    
+        })
+        return message.channel.send(`\`${args[0]}\` reddedildi. Gerekli kişiye bilgilendirme yapıldı.`)
+    })
+}
+
+
