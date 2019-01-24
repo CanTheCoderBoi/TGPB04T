8c75724417bbff26589a0209ad8bdc6e5d2acf81
Can <canerdal2003@hotmail.com>
2019-01-25 00:00:59 +0300

    CREAN sucks

--- cmds/apuan.js
+++ cmds/apuan.js
@@ -6,11 +6,11 @@ module.exports.run = (client, message, args) => {
     if(!client.owners.includes(message.author.id)) return;
     let member = shortcuts.getMember(message, args);
     if (!member) return message.reply("Hmm");
-    if (Number(args[0])) return message.reply("Hmm");
+    if (!Number(args[1])) return message.reply("Hmm");
     schemas.userPoints.findOne({userID: member.id}, (err, Dmember) => {
-        Dmember.points += parseInt(args[0]);
+        Dmember.points += parseInt(args[1]);
         Dmember.save();
-        return message.channel.send(`:ok_hand: **${member.user.tag}** + ${args[0]} = ${Dmember.points}`);
+        return message.channel.send(`:ok_hand: **${member.user.tag}** + ${args[1]} = ${Dmember.points}`);
     })
 }
 
