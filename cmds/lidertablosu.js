93c8fc06eaaff603be79803d48ff1104424a9915
Can <canerdal2003@hotmail.com>
2019-01-25 00:55:48 +0300

    Test lidertablosu.js

--- cmds/lidertablosu.js
+++ cmds/lidertablosu.js
@@ -1,15 +1,14 @@
 const Discord = require("discord.js");
 const shortcuts = require("../utils/shortcuts.js");
 const schemas = require("../schemas.js")
-const {table} = require("table");
 module.exports.run = (client, message, args) => {
-    data = [["Sıralama", "Kullanıcı", "Puanı"]]
+    let data = []
     schemas.userPoints.find({}).sort('-points').limit(10).exec((err, results) => {
         let leaderBoardEmbed = new Discord.RichEmbed().setTitle("Lider Tablosu:").setTimestamp().setFooter(client.user.tag, client.user.avatarURL).setColor(0x9f6f78);
         for (let i = 0; i < results.length; i++) {
-            data.push(`#${ i+ 1}| ${message.guild.members.get(results[i].userID).user.tag} | ${ results[i].points }<:tgpcoin:530810516629618718>`);
+            data.push(`#${ i+ 1} ${message.guild.members.get(results[i].userID).user.tag} | ${ results[i].points }<:tgpcoin:530810516629618718>`);
         }
-        leaderBoardEmbed.setDescription(table(data, config));
+        leaderBoardEmbed.setDescription(data.join("\n"));
         message.channel.send(leaderBoardEmbed);
     })
 }
