8ebea8927c343b33ab265410d8c6fe65fac03de9
Can <canerdal2003@hotmail.com>
2019-01-24 23:55:48 +0300

    Toasty sucks

--- cmds/apuan.js
+++ cmds/apuan.js
@@ -8,7 +8,8 @@ module.exports.run = (client, message, args) => {
     if (!member) return message.reply("Hmm");
     if (Number(args[0])) return message.reply("Hmm");
     schemas.userPoints.findOne({userID: member.id}, (err, Dmember) => {
-        Dmember.points += parseInt(Math.floor(args[0]));
+        Dmember.points += parseInt(args[0]);
+        Dmember.save();
         return message.channel.send(`:ok_hand: **${member.user.tag}** + ${args[0]} = ${Dmember.points}`);
     })
 }
