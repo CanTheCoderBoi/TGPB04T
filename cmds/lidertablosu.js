3dcbdf25edeb932b6aa2572c955dc07eb3e78cf7
Can <canerdal2003@hotmail.com>
2019-01-25 00:21:11 +0300

    Test lidertablosu.js

---
+++ cmds/lidertablosu.js
@@ -0,0 +1,19 @@
+const Discord = require("discord.js");
+const shortcuts = require("../utils/shortcuts.js");
+const schemas = require("../schemas.js")
+
+module.exports.run = (client, message, args) => {
+    schemas.userPoints.find({}).sort({userID: "asc"}).limit(10).exec((err, results) => {
+        let leaderBoardEmbed = new Discord.RichEmbed().setTitle("Lider Tablosu:").setTimestamp().setFooter(client.user.tag, client.user.avatarURL);
+        let text = [];
+        for (let i = 0; i < results.length; i++) {
+            text.push(`${i}. <@${results[i].userID}> | ${results[i].points}<:tgpcoin:530810516629618718>`);
+        }
+        leaderBoardEmbed.setDescription(`\`\`\`${text.join("\n")}\`\`\``);
+        message.channel.send(leaderBoardEmbed);
+    })
+}
+
+module.exports.info = {
+    DM: false
+}
