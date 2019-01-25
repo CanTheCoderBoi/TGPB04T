e459b3b779b866413b402af59cc4a4b02772bfc5
Can <canerdal2003@hotmail.com>
2019-01-25 16:23:21 +0300

    Fixed some stuff

--- cmds/duyuru.js
+++ cmds/duyuru.js
@@ -3,7 +3,7 @@ module.exports.run = (client, message, args) => {
 
 
         message.delete()
-        if(!message.member.hasPermission("ADMINISTRATOR")) return message.reply("Bunu kullanmak için yetkiniz yok")
+   if (!message.member.hasPermission("ADMINISTRATOR") || message.author.id != "437286229403238410") return message.reply("Bunu kullanmak için yetkiniz yok")
         var dtext = args.join(' ')
         if(!dtext) return message.reply('Metin girişi yapılmadı! Lütfen metin giriniz.')
         
