763cf9951c0ef4c0bbe708df89a68d0b18835844
Can <canerdal2003@hotmail.com>
2019-01-26 23:20:20 +0300

    düzeltmeler

--- cmds/envanter.js
+++ cmds/envanter.js
@@ -7,7 +7,7 @@ module.exports.run = async (client, message, args) => {
     let user = await schemas.userPoints.findOne({userID: message.author.id});
     let invEmbed = new Discord.RichEmbed()
     .setTitle(`${message.author.tag} Envanterin:`)
-    .setDescription(`\`\`\`${user.inv.join("\n").toUperCase()}\`\`\``)
+    .setDescription(`\`\`\`${user.inv.join("\n").toUpperCase()}\`\`\``)
     .setColor(0x4fC312)
     .setTimestamp()
     message.channel.send(invEmbed);
