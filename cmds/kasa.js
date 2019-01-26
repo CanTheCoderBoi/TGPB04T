cfceb150f5a4c9ebb7f54508d6c88ce99ae34b37
Can <canerdal2003@hotmail.com>
2019-01-26 23:08:56 +0300

    BİTTİ BE OLEY BE

--- cmds/kasa.js
+++ cmds/kasa.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const schemas = require("../schemas.js");
 const items = require("../items.json");
 const shortcuts = require("../utils/shortcuts.js")
-module.exports.run = (client, message, args) => {
+module.exports.run = async (client, message, args) => {
     schemas.userPoints.findOne({ userID: message.author.id }).then(user => {
         if (!user) {
             let newUser = new schemas.userPoints({ userID: message.author.id })
@@ -14,6 +14,7 @@ module.exports.run = (client, message, args) => {
             if (!items.kasalar[item]) return message.reply("Üzgünüm ama böyle bir kasa bulunmamaktadır.");
             if (user.points < items.kasalar[item].price) return message.channel.send("Üzgünüm ama yeterli miktarda puanınız bulunmamaktadır.");
             user.points -= items.kasalar[item].price;
+            await user.save();
             shortcuts.kasaAc(message, item);
 
         }
