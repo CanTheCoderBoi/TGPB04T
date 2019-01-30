79264fe306ba751035a8f53a420dfac5ee8a0b58
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-30 13:03:30 +0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -2,10 +2,9 @@ const discord = require("discord.js");
 module.exports.run = (client, message, args) => {
 
 let bankisi = message.mentions.members.first()
-let baankisi = message.mentions.users.first()
 if(!bankisi.bannable) return message.reply(" Bu kişiyi yasaklayamazsınız.")
 if(!message.member.roles.has("523179624772010004")) return message.channel.send("Bu komudu kullanmak için yetkiniz yok.")
-if(!baankisi) return message.channel.send("Komut kullanımı yanlış (`<kullanıcı>| [Sebep:...]`)");
+if(!bankisi) return message.channel.send("Komut kullanımı yanlış (`<kullanıcı>| [Sebep:...]`)");
 let bansebep = args.slice(1).join(" ")
 
 message.channel.send("Başarıyla **" + bankisi.user.tag + "** (`"+bankisi.id+"`) kullanıcısını sunucudan yasakladım." )
