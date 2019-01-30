edefe5f7abcfe357a44ba9e5df000380cbdc7747
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-01-30 13:44:41 +0300

    Update ban.js

--- cmds/ban.js
+++ cmds/ban.js
@@ -3,27 +3,19 @@ module.exports.run = (client, message, args) => {
     let hedef = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     let sebep = args.slice(1).join(' ');
     if (!message.member.roles.has('523179624772010004')) return message.channel.send(':lock: Bu komudu kullanmak için yetkiniz yok.');
-    if (!hedef) return message.reply("Komut kullanımı yanlış. (`user:<user>| [sebep...]`)");
-    
+    if (!hedef) return message.channel.send("Komut kullanımında eksik argüman girildi. (`user:<user>| [sebep...]`)");
+    if(hedef.roles.has("523179624772010004") return message.channel.send ("Bu kişiyi yasaklayamazsınız.")
 
     
 
     let bembed = new discord.RichEmbed()
 
         .setColor('RANDOM')
-
         .setThumbnail(hedef.user.avatarURL)
-
         .addField('Banlanan üye', `${hedef.user.username} (${hedef.user.id})`)
-
         .addField('Banlayan moderatör', `${message.author.username} (${message.author.id})`)
-
-        .addField('Banlama zamanı', message.createdAt)
-
         .addField('Komudun kullanıldığı kanal', message.channel)
-
-        .addField('Ban sebebi', sebep)
-        .setFooter('Banlanan üye hakkında', hedef.user.displayAvatarURL);
+        .addField('Ban sebebi', sebep)      
 
 
     message.channel.send(`${hedef.user.username} adlı kullanıcı sunucudan yasaklandı.`);
