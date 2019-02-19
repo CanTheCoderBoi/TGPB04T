6d0649e9be0ab415757bf3362fffed76b06ffb45
thefurkanozkan <46135095+thefurkanozkan@users.noreply.github.com>
2019-02-19 23:53:24 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,7 +11,7 @@ client.on("ready", () => {
 })
 
 client.on("guildMemberAdd", async member => {
-    client.channels.get("527501064879210506").send(`Selam <@${member.id}>! Sunucuya hoşgeldin`)
+    client.channels.get("527501064879210506").send(`<a:selam:547520182768238601> Selam <@${member.id}>, sunucuya hoş geldin!`)
     let role = client.guilds.get("403879632656662528").roles.get("523183261518921728");
     member.addRole(role);
 })
